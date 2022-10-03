c620a54f1600de62330e7f4f3fe548f72dc783ca
Venkatesan15 <101502792+Venkatesan15@users.noreply.github.com>
2022-10-03 12:54:38 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,246 +1,183 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        int userinput = 1;
-        int regorlog;
-        String name,gender,phno,pass;
-        int age;
         Scanner sc = new Scanner(System.in);
-        List<register> Register = new ArrayList<>();
-        System.err.println("WELCOME TO ATM");
-        while (userinput == 1) {
-            System.out.println("use ATM---->1");
-            System.out.println("Quit---->2");
-            userinput = sc.nextInt();
-            if (userinput == 1) {
-                System.out.println("Register--->1");
-                System.out.println("Login------>2");
-                regorlog = sc.nextInt();
-                if (regorlog == 1) {
-                    name=register.NAME();
-                    age=register.AGE();
-                    gender=register.Gender();
-                    phno=register.PHNO(Register);
-                    pass=register.PASS();
-                    Register.add(new register(name,age,gender,phno,pass,0));
-                    System.out.println("Registration Successful");
-                }
-                 else if (regorlog == 2) {
-                     venkat.methode(Register);
-                }
-            }
-        }
-        if(userinput==2)
-            System.out.println("HAVE A GOOD DAY");
-    }
-}
-class register extends Main
-{
-    String name,gender,phno,pass;
-    int age;
+        List<store> Store=new ArrayList<>();
+        List<bill> billa=new ArrayList<>();
+        int kgwant;
+        int amount1=0;
 
-    Main ob=new Main();
-    int acbalence;
-    public static String PASS()
-    {
-        Scanner sc=new Scanner(System.in);
-        String pass,pass1;
-        System.out.println("Please enter ur password");
-        pass=sc.nextLine();
-        System.out.println("Please reenter the password");
-        pass1=sc.nextLine();
-        while(!(pass.equals(pass1)))
-        {
-            System.err.println("your password is not same first time u enter");
-            System.out.println("Please reenter the password");
-            pass1=sc.nextLine();
-        }
-        return pass;
-    }
-    public static String Gender()
-    {
-        String gender;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Please enter ur gender(male or female)");
-        gender=sc.nextLine();
-        if(gender.equals("male")||gender.equals("female"))
-            return gender;
-        else {
-            System.err.println("Please enter valid gender");
-            gender=Gender();
-            return gender;
-        }
+        Store.add(new store("sugar",20,30));
+        Store.add(new store("salt",30,20));
+        Store.add(new store("t.paruppu",20,50));
+        Store.add(new store("masala",20,45));
+        Store.add(new store("Rise",20,40));
+        Store.add(new store("k.paruppu",20,35));
+        Store.add(new store("turmeric powder",20,70));
+        Store.add(new store("vellam",20,90));
+        store obj1=Store.get(0);
+        store obj2=Store.get(1);
+        store obj3=Store.get(2);
+        store obj4=Store.get(3);
+        store obj5=Store.get(4);
 
-    }
-    public static String PHNO(List<register> Register)
-    {
-        int C=1,C1=0;
-        Scanner sc=new Scanner(System.in);
-        String  phno;
-        System.out.println("Please enter ur phone number must be 10 digits");
-        phno=sc.nextLine();
-        if((phno.length()==10))
-            C1=1;
-        if(C1==1)
+
+        store obj6=Store.get(5);
+        store obj7=Store.get(6);
+        store obj8=Store.get(7);
+
+
+
+        int useroption=1;
+        int itemwant;
+        int AMOUNT=0;
+        while(useroption==1)
         {
-            for(register i:Register)
+            System.out.println("if u want to buy---> 1");
+            System.out.println("exit---> 2");
+            useroption=sc.nextInt();
+            if(useroption==1)
             {
-                if((i.phno).equals(phno)) {
-                    System.out.println("This ph number is already taken ");
-                    C=0;
-                    break;
+                System.out.println("sugar--->1:price : "+obj1.price+" Available : "+obj1.Kg+" kg");
+                System.out.println("salt--->2:price : "+obj2.price+" Available : "+obj2.Kg+" kg");
+                System.out.println("t.paruppu--->3 :price : "+obj3.price+" Available : "+obj3.Kg+" kg");
+                System.out.println("masala--->4 :price : "+obj4.price+" Available : "+obj4.Kg+" kg");
+                System.out.println("Rise--->5 :price : "+obj5.price+" Available : "+obj5.Kg+" kg");
+                System.out.println("k.paruppu--->6 :price : "+obj6.price+" Available : "+obj6.Kg+" kg");
+                System.out.println("turmeric powder--->7:price : "+obj7.price+" Available : "+obj7.Kg+" kg");
+                System.out.println("vellam--->8:price : "+obj8.price+" Available : "+obj8.Kg+" kg");
+                System.out.println("which item u want");
+                itemwant=sc.nextInt();
+                if(itemwant>8)
+                {
+                    System.out.println("please enter valid result");
                 }
+                else
+                {
+                    System.out.println("enter how much kg u want");
+                    kgwant = sc.nextInt();
+                    switch (itemwant) {
+                        case 1:
+                            if (obj1.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj1.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj1.Kg -= kgwant;
+                                billa.add(new bill(obj1.item,obj1.price,kgwant,kgwant*obj1.price));
 
-            }
-        }
-        if(C1==0||C==0)
-        {
-            phno=PHNO(Register);
-        }
+                            } else {
+                                System.out.println("sorry we have only " + obj1.Kg + " kg in our store");
+                            }
 
-        return phno;
-    }
-    public static int AGE()
-    {
-        Scanner sc=new Scanner(System.in);
-        int age;
-        System.out.println("Please enter ur age");
-        System.out.println("your age is must be higher than 18");
-        age=sc.nextInt();
-        if(age>17)
-            return age;
-        else
-        {
-            System.err.println("sorry we could not create an account below 18");
-            age=AGE();
-            return age;
-        }
-    }
-    public static String NAME()
-    {
-        String firstname;
-        int FNC=0,LNC=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Please enter ur name in only upper case without any special characters except space");
-        firstname=sc.nextLine();
-
-        for(char i:firstname.toCharArray())
-        {
-            if((i>=65&&i<=90)||i==' ')
-            {
-                FNC++;
-            }
-        }
-        if(FNC==firstname.length())
-            return firstname;
-        else
-        {
-            System.err.println("Please enter valid name");
-            firstname=NAME();
-            return firstname;
-        }
-
-    }
-    register(String name,int age,String gender,String phno,String pass,int acbalence)
-    {
-        this.name=name;
-        this.age=age;
-        this.gender=gender;
-        this.phno=phno;
-        this.pass=pass;
-        this.acbalence=acbalence;
-    }
-}
-class afterlogin
-{
-
-    public static void addmoney(register i)
-    {
-        Scanner sc=new Scanner(System.in);
-        int amountadd;
-        System.out.println("Your account balance is "+i.acbalence);
-        System.out.println("Please enter amount you add");
-        amountadd=sc.nextInt();
-        i.acbalence+=amountadd;
-        System.out.println("ur account balance is "+i.acbalence);
-    }
-    public static void withdraw(register i)
-    {
-        System.out.println("Please enter how much money u want");
-        int WD;
-        Scanner sc=new Scanner(System.in);
-        WD=sc.nextInt();
-        if(WD>i.acbalence)
-            System.out.println("Your account balence is only "+i.acbalence);
-        else {
-            System.out.println("Withdraw successful");
-            i.acbalence-=WD;
-            System.out.println("Your current balance is after withdraw "+i.acbalence);
-        }
-    }
-    public static void changepass(register i)
-    {
-        i.pass=register.PASS();
-        System.out.println("The password is set Successfully");
-    }
-}
-class venkat
-{
-    public static void methode(List<register> Register)
-    {
-        Scanner sc=new Scanner(System.in);
-        String phno,pass;
-        int count=0,UIafterlogin;
-        System.out.println("Please enter ur phone number");
-        phno = sc.nextLine();
-        System.out.println("Please enter ur password");
-        pass = sc.nextLine();
-        count=loginpage(Register,phno,pass,count);
-        if (count == 0) {
-            System.out.println("Sorry we could not find ur account");
-        }
-
-    }
-    public static int loginpage(List<register> Register, String phno, String pass, int count)
-    {
-        for (register i : Register) {
-            if ((i.phno).equals(phno) && (i.pass).equals(pass)) {
-                count=1;
-                Scanner sc = new Scanner(System.in);
-                int UIafterlogin=1;
-                while(UIafterlogin!=5) {
-                    System.out.println("if u want add money--->1");
-                    System.out.println("if u want check balance--->2");
-                    System.out.println("if u want withdraw money--->3");
-                    System.out.println("if u want to change ur password--->4");
-                    System.out.println("exit----->5");
-                    UIafterlogin = sc.nextInt();
-                    if(UIafterlogin<5) {
-                        switch (UIafterlogin) {
-                            case 1:
-                                afterlogin.addmoney(i);
-                                break;
-                            case 2:
-                                System.out.println("Your account Balance is " + i.acbalence);
-                                break;
-                            case 3:
-                                afterlogin.withdraw(i);
-                                break;
-                            case 4:
-                                afterlogin.changepass(i);
-                                break;
-                            default:
-                                System.out.println("Sorry we could not help u");
-                        }
+                            break;
+                        case 2:
+                            if (obj2.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj2.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj2.Kg -= kgwant;
+                                billa.add(new bill(obj2.item,obj2.price,kgwant,kgwant*obj2.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj2.Kg + " kg in our store");
+                            }
+                            break;
+                        case 3:
+                            if (obj3.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj3.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj3.Kg -= kgwant;
+                                billa.add(new bill(obj3.item,obj3.price,kgwant,kgwant*obj3.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj3.Kg + " kg in our store");
+                            }
+                            break;
+                        case 4:
+                            if (obj4.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj4.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj4.Kg -= kgwant;
+                                billa.add(new bill(obj4.item,obj4.price,kgwant,kgwant*obj4.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj4.Kg + " kg in our store");
+                            }
+                            break;
+                        case 5:
+                            if (obj5.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj5.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj5.Kg -= kgwant;
+                                billa.add(new bill(obj5.item,obj5.price,kgwant,kgwant*obj5.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj5.Kg + " kg in our store");
+                            }
+                            break;
+                        case 6:
+                            if (obj6.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj6.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj6.Kg -= kgwant;
+                                billa.add(new bill(obj6.item,obj6.price,kgwant,kgwant*obj6.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj6.Kg + " kg in our store");
+                            }
+                            break;
+                        case 7:
+                            if (obj7.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj7.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj7.Kg -= kgwant;
+                                billa.add(new bill(obj7.item,obj7.price,kgwant,kgwant*obj7.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj7.Kg + " kg in our store");
+                            }
+                            break;
+                        case 8:
+                            if (obj8.Kg >= kgwant) {
+                                AMOUNT += kgwant * obj8.price;
+                                System.out.println(kgwant + " kilo gram added in ur cart successfully");
+                                obj8.Kg -= kgwant;
+                                billa.add(new bill(obj8.item,obj8.price,kgwant,kgwant*obj8.price));
+                            } else {
+                                System.out.println("sorry we have only " + obj8.Kg + " kg in our store");
+                            }
+                            break;
+                        default:
+                            System.out.println("please enter valid list");
                     }
                 }
-                break;
+                }
+
             }
+        for(bill i:billa)
+        {
+            System.out.println("Item : "+i.itemName+",kilo : "+i.kg+" ,price per kg : " +i.prise+" ,total : "+i.total);
         }
 
-        return count;
+        System.err.println("your total bill is : "+AMOUNT);
+        }
+}
+class store
+{
+    String item;
+    int Kg;
+    int price;
+    public store(String Item,int Kg,int price) {
+        this.item= Item;
+        this.Kg=Kg;
+        this.price=price;
+    }
+
+}
+class bill
+{
+    String itemName;
+    int prise;
+    int kg;
+    int total;
+
+    public bill(String item, int price, int kgwant,int total) {
+        this.itemName=item;
+        this.prise=price;
+        this.kg=kgwant;
+        this.total=total;
     }
 }
-
-
 
